937e33b1861010736016a7dd0025d8f5121f92a7
StangBang03 <158178163+StangBang03@users.noreply.github.com>
2024-07-14 10:50:24 -0400

    Results, Record, Dropdown Border

--- assets/js/results.js
+++ assets/js/results.js
@@ -3,57 +3,97 @@ $(document).ready(function() {
     var sport = 'baseball'; // Change this to your desired sport
     var team = 'dmvvandals'; // Change this to your team name
     var password = 'Pa$$word1!'; // Change this to your password
-  
+
+    // Create dropdown menu
+    var dropdownHtml = `
+        <select id="leagueSelect">
+            <option value="9A1BA060-2FC1-4C7F-B9F6-CB70F122FFBF">2024 Spring/Summer</option>
+            <option value="294CA57A-307B-48FB-BD2C-9B8256C0EE13" selected>2024 Spring/Summer Playoffs</option>
+        </select>
+    `;
+    $('#dropdownContainer').html(dropdownHtml);
+
+    // Set initial league value
+    var league = $('#leagueSelect').val();
+
     // Initial data retrieval and table rendering
-    $.ajax({
-      type: "GET",
-      url: "https://api.iscoresports.com/teamwebsite/games.php",
-      data: { s: sport, t: team, p: password },
-      dataType: "jsonp",
-      success: function(data) {
-        showResults(data);
-      },
-      error: function() {
-        alert("An error occurred in the request");
-      }
+    function fetchData(){
+        $.ajax({
+            type: "GET",
+            url: "https://api.iscoresports.com/teamwebsite/games.php",
+            data: { s: sport, t: team, p: password, lg: league},
+            dataType: "jsonp",
+            success: function(data) {
+                showResults(data);
+            },
+            error: function() {
+                alert("An error occurred in the request");
+            }
+        });
+    }
+    
+    // Fetch data initially
+    fetchData();
+
+    // Update league value and fetch data on dropdown change
+    $('#leagueSelect').change(function() {
+        league = $(this).val();
+        fetchData();
     });
-  
+
     function showResults(data) {
-      var games = $(data).find('GAME');
-      
-      var html = '<table>';
-      // Table header
-      html += '<tr><th>Date</th><th>Matchup<th>Score</th><th>Result</th></tr>';
-  
-      // Loop through each game
-      games.each(function() {
-        var game = $(this);
-        var nameWithDate = game.attr('name') || '';
-        console.log("Name with Date:", nameWithDate);  // Log the nameWithDate to check its format
+        var games = $(data).find('GAME');
 
-        var visitor = game.attr('visitor') || '';
-        var home = game.attr('home') || '';
-        var visitorScore = game.attr('visitorScore') || 0;
-        var homeScore = game.attr('homeScore') || 0;
-        var result = game.attr('result') || '';
+        var html = '<table>';
+        // Table header
+        html += '<tr><th>Date</th><th>Matchup<th>Score</th><th>Result</th></tr>';
 
-        // Extract date from the beginning of the name using regex
-        var nameParts = nameWithDate.match(/(\d{1,2}\/\d{1,2}\/\d{2})\s+(.+)/);
-        var date = nameParts ? nameParts[1] : '';
-        var name = nameParts ? nameParts[2] : nameWithDate;
-        console.log("Extracted Name:", name);  // Log the extracted name
-        console.log("Extracted Date:", date);  // Log the extracted date
-  
-        // Construct table rows
-        html += '<tr>';
-        html += '<td>' + date + '</td>';
-        html += '<td>' + name + '</td>';
-        html += '<td>' + (visitorScore + ' - ' + homeScore) + '</td>';
-        html += '<td>' + result + '</td>';
-        html += '</tr>';
-      });
-  
-      html += '</table>';
-      $('#results').html(html);
-    } 
+        // Counters for wins, losses, and ties
+        var wins = 0;
+        var losses = 0;
+        var ties = 0;
+
+        // Loop through each game
+        games.each(function() {
+            var game = $(this);
+            var nameWithDate = game.attr('name') || '';
+            var visitor = game.attr('visitor') || '';
+            var home = game.attr('home') || '';
+            var visitorScore = parseInt(game.attr('visitorScore')) || 0;
+            var homeScore = parseInt(game.attr('homeScore')) || 0;
+            var result = game.attr('result') || '';
+
+            // Extract date from the beginning of the name using regex
+            var nameParts = nameWithDate.match(/(\d{1,2}\/\d{1,2}\/\d{2})\s+(.+)/);
+            var date = nameParts ? nameParts[1] : '';
+            var name = nameParts ? nameParts[2] : nameWithDate;
+
+            // Update counters based on the result
+            if (result.toLowerCase() === 'win') {
+                wins++;
+            } else if (result.toLowerCase() === 'loss') {
+                losses++;
+            } else if (result.toLowerCase() === 'tie') {
+                ties++;
+            }
+
+            // Construct table rows
+            html += '<tr>';
+            html += '<td>' + date + '</td>';
+            html += '<td>' + name + '</td>';
+            html += '<td>' + (visitorScore + ' - ' + homeScore) + '</td>';
+            html += '<td>' + result + '</td>';
+            html += '</tr>';
+        });
+
+        html += '</table>';
+
+        // Display the results in the desired format
+        var resultText = '<div id="resultsText">' + '&nbsp;' + 'Record: ' + wins + ' - ' + losses + ' - ' + ties + '</div>';
+        $('#results').html(html + resultText);
+    }
+
+    // Add CSS for left-aligning the results and adding a margin
+    var css = '<style>#resultsText { text-align: left; margin-top: 10px; }</style>';
+    $('head').append(css);
 });
